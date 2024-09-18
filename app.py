7ea0530548ff18a6cdbd94e9c9cf1842923cb8ac
kazaneza <kazanezagentil@gmail.com>
2024-09-18 15:08:51 +0200

    added different graphs

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-from graph.graph_generator import create_graph
+from graph.graph1 import create_graph_failure
+from graph.graph2 import create_graph_success
+from graph.graph3 import create_graph_pending
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    graph_url = create_graph()
-    return render_template('dashboard.html', graph_url=graph_url)
+    graph_failure_url = create_graph_failure() 
+    graph_success_url = create_graph_success() 
+    graph_pending_url = create_graph_pending() 
+    return render_template('dashboard.html',create_graph_pending=graph_pending_url, create_graph_success=graph_success_url, create_graph_failure=graph_failure_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ graph/graph3.py
@@ -0,0 +1,31 @@
+import matplotlib
+matplotlib.use('Agg') 
+
+import matplotlib.pyplot as plt
+import io
+import base64
+
+def create_graph_pending():
+    fig, ax = plt.subplots()
+
+    ax.plot([1, 2, 3, 4], [1, 4, 9, 16], color='white', linewidth=2.5)
+
+    ax.set_xlabel('X Axis', color='white')
+    ax.set_ylabel('Y Axis', color='white')
+   
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+ 
+    ax.spines['top'].set_color('none')
+    ax.spines['right'].set_color('none')
+ 
+    ax.grid(color='white', linestyle='--', linewidth=0.5)
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png', bbox_inches='tight', transparent=True)  
+    img.seek(0) 
+    graph_url_pending = base64.b64encode(img.getvalue()).decode()
+  
+    plt.close()
+
+    return f"data:image/png;base64,{graph_url_pending}"
